f830491fdcfc18b93717a32732feffc20f86b9fb
sanggon <sanggon6107@naver.com>
2020-07-24 10:51:33 +0900

    chap08 제네릭의 이해

---
+++ kotlinProgramming/Module1/src/chap08/section1/generic.kt
@@ -0,0 +1,15 @@
+package chap08.section1
+
+class Box<T>(t : T){
+    var name = t
+}
+
+
+fun main(){
+    val box1 : Box<Int> = Box<Int>(1)
+    val box2 : Box<String> = Box<String>("string.")
+
+    println("box1.name : ${box1.name}")
+    println("box2.name : ${box2.name}")
+}
+
